9e74368754a860a5446c0301ee93de0af7ec2d1c
Steve Jabour <steve@jabour.me>
2021-01-28 11:32:36 -0700

    pass options correctly

--- lib/peak/function/simple.rb
+++ lib/peak/function/simple.rb
@@ -4,8 +4,8 @@ module Peak
   module Function
     class Simple
       class << self
-        def authenticate(token, options)
-          return Peak::JsonWebToken.decode(token)
+        def authenticate(token, **options)
+          return Peak::JsonWebToken.decode(token, **options)
         rescue JWT::JWKError => exception
           logger.warn("JWT::JWKError #{exception}")
           logger.error(exception.backtrace.join("\n"))
