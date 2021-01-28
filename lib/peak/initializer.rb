2772fe9e3212bc533c5f31416e714f48f8b7c7ab
Steve Jabour <steve@jabour.me>
2021-01-28 12:17:49 -0700

    update initializer from latest rom

--- lib/peak/initializer.rb
+++ lib/peak/initializer.rb
@@ -3,46 +3,62 @@
 require 'dry/initializer'
 
 module Peak
+  # @api private
   module Initializer
+    # @api private
     module DefineWithHook
+      # @api private
       def param(*)
-        super.tap do
-          __define_with__
-        end
+        super.tap { __define_with__ }
       end
+      ruby2_keywords(:param) if respond_to?(:ruby2_keywords, true)
 
+      # @api private
       def option(*)
         super.tap do
           __define_with__ unless method_defined?(:with)
         end
       end
+      ruby2_keywords(:option) if respond_to?(:ruby2_keywords, true)
 
+      # @api private
       def __define_with__
-        seq_names = dry_initializer.definitions.reject { |_, d| d.option }.keys.join(', ')
+        seq_names = dry_initializer
+          .definitions
+          .reject { |_, d| d.option }
+          .keys
+          .join(", ")
 
-        seq_names << ', ' unless seq_names.empty?
+        seq_names << ", " unless seq_names.empty?
 
         undef_method(:with) if method_defined?(:with)
 
         class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
-          def with(new_options = EMPTY_HASH)
+          def with(**new_options)
             if new_options.empty?
               self
             else
-              self.class.new(#{ seq_names }options.merge(new_options))
+              self.class.new(#{seq_names}**options, **new_options)
             end
           end
         RUBY
       end
     end
 
+    # @api private
     def self.extended(base)
       base.extend(Dry::Initializer[undefined: false])
       base.extend(DefineWithHook)
       base.include(InstanceMethods)
     end
 
+    # @api private
     module InstanceMethods
+      # Instance options
+      #
+      # @return [Hash]
+      #
+      # @api public
       def options
         @__options__ ||= self.class.dry_initializer.definitions.values.each_with_object({}) do |item, obj|
           obj[item.target] = instance_variable_get(item.ivar)
@@ -50,9 +66,11 @@ module Peak
       end
 
       define_method(:class, Kernel.instance_method(:class))
-
       define_method(:instance_variable_get, Kernel.instance_method(:instance_variable_get))
 
+      # This makes sure we memoize options before an object becomes frozen
+      #
+      # @api public
       def freeze
         options
         super
