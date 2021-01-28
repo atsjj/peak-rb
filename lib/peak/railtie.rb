fae30d3b2761e8ac2aa8e449c11ec55c182c4949
Steve Jabour <steve@jabour.me>
2021-01-28 11:07:25 -0700

    fixing config for railtie

--- lib/peak/railtie.rb
+++ lib/peak/railtie.rb
@@ -7,16 +7,11 @@ module Peak
     include Dry::Core::Constants
     include Peak::Env::Constants
 
-    config.peak = ActiveSupport::OrderedOptions.new
-    config.peak.env.environment = ENV
-    config.peak.env.namespace = EMPTY_STRING
-    config.peak.env.prefix = EMPTY_STRING
-    config.peak.env.target = DEFAULT_TARGET
+    config.peak = Peak::Configuration.config
 
     config.eager_load_namespaces << Peak
 
     initializer 'peak.env', before: :load_environment_hook do |app|
-      Peak.config.env.environment = app.config.peak.env.environment
       Peak.config.env.namespace = app.railtie_name.sub(APP_NAME_PATTERN, EMPTY_STRING)
       Peak.config.env.prefix = app.config.peak.env.prefix
       Peak.config.env.target = Rails.env
