48fcd516258080f1a213426f1c595ff1ade3ae31
Steve Jabour <steve@jabour.me>
2021-08-13 16:28:13 -0600

    removed logger calls

--- lib/peak/function/simple.rb
+++ lib/peak/function/simple.rb
@@ -7,16 +7,10 @@ module Peak
         def authenticate(token, **options)
           return Peak::JsonWebToken.decode(token, **options)
         rescue JWT::JWKError => exception
-          logger.warn("JWT::JWKError #{exception}")
-          logger.error(exception.backtrace.join("\n"))
           false
         rescue JWT::DecodeError => exception
-          logger.warn("JWT::DecodeError #{exception}")
-          logger.error(exception.backtrace.join("\n"))
           false
         rescue Exception => exception
-          logger.warn("Peak::Function::Simple #{exception}")
-          logger.error(exception.backtrace.join("\n"))
           false
         end
       end
